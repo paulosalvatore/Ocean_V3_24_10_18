89a2dde0a6156449216cd99d0adf316e793a0654
Paulo Salvatore <salvatoregames@gmail.com>
2018-11-07 08:02:41 -0200

    Initial commit

---
+++ Assets/TrocarImagem.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TrocarImagem : MonoBehaviour
+{
+	public Material material1;
+	public Material material2;
+
+	private void Start()
+	{
+		InvokeRepeating("AlterarSkybox", 20f, 20f);
+	}
+
+	private void AlterarSkybox()
+	{
+		RenderSettings.skybox = RenderSettings.skybox == material1 ? material2 : material1;
+	}
+}
